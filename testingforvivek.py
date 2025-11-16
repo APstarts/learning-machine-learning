58bdebfeecf9db4bc0b1aac38928c43ab22efa9e
Anurag Pal <tanmaypal123@outlook.com>
2025-11-16 12:13:38 +0530

    added a new function

--- testingforvivek.py
+++ testingforvivek.py
@@ -7,3 +7,6 @@ def calculator(x: int,y: int, operator: str):
         return x * y
     else:
         return x + y
+    
+def record_creator(name: str, surname:str, age: int):
+    return name + surname + str(age)
